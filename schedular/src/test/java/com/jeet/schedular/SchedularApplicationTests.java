4e313aebb78f288b904e29faf2d4bb10dff13133
Jeetendra Singh <chauhanjeetendra@gmail.com>
2024-12-25 16:47:58 +0530

    Init project

---
+++ schedular/src/test/java/com/jeet/schedular/SchedularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeet.schedular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
