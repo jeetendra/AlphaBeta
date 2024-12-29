b9c277d35f18706ef1958394be056d92f7610b03
Jeetendra Singh <chauhanjeetendra@gmail.com>
2024-12-29 13:03:02 +0530

    init instapost

---
+++ InstaPost/src/main/java/com/jeet/instapost/InstaPostApplication.java
@@ -0,0 +1,13 @@
+package com.jeet.instapost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaPostApplication.class, args);
+    }
+
+}
---
+++ InstaPost/src/test/java/com/jeet/instapost/InstaPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeet.instapost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
