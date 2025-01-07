15e178bdb1484a67f346fc2aa7c3ca8668b57ded
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-01-07 22:58:06 +0530

    Added Websocket handler

---
+++ ChatConsumer/src/main/java/com/jeet/chatconsumer/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.jeet.chatconsumer.config;
+
+import com.jeet.chatconsumer.handler.ChatWebSocketHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public HandlerMapping webSocketHandlerMapping(ChatWebSocketHandler chatHandler) {
+        Map<String, WebSocketHandler> map = new HashMap<>();
+        map.put("/ws/chat", chatHandler);
+
+        SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
+        handlerMapping.setUrlMap(map);
+        handlerMapping.setOrder(-1);
+
+        return handlerMapping;
+    }
+}
