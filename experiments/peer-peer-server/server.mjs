ac7e30d9d58b93a5d1c770f24f80a488c2ef4990
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-01-20 12:31:18 +0530

    changed in peer server to broadcast change to other peers

--- experiments/peer-peer-server/server.mjs
+++ experiments/peer-peer-server/server.mjs
@@ -1,29 +1,27 @@
-const express = require('express');
-const { WebSocketServer, WebSocket } = require('ws');
-const { v4: uuidv4 } = require('uuid');
+import { createServer } from 'https';
+import WebSocket, { WebSocketServer } from 'ws';
+import { readFileSync } from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
-const wss = new WebSocketServer({
-    port: 3000,
-    perMessageDeflate: {
-      zlibDeflateOptions: {
-        // See zlib defaults.
-        chunkSize: 1024,
-        memLevel: 7,
-        level: 3
-      },
-      zlibInflateOptions: {
-        chunkSize: 10 * 1024
-      },
-      // Other options settable:
-      clientNoContextTakeover: true, // Defaults to negotiated value.
-      serverNoContextTakeover: true, // Defaults to negotiated value.
-      serverMaxWindowBits: 10, // Defaults to negotiated value.
-      // Below options specified as default values.
-      concurrencyLimit: 10, // Limits zlib concurrency for perf.
-      threshold: 1024 // Size (in bytes) below which messages
-      // should not be compressed if context takeover is disabled.
-    }
-  });
+const server = createServer({
+    cert: readFileSync('../cert/cert.pem'),
+    key: readFileSync('../cert/key.pem')
+});
+
+const wss = new WebSocketServer({ server });
+
+// wss.on('connection', function connection(ws) {
+//     ws.on('error', console.error);
+//     ws.on('message', (data, isBinary) => {
+//         wss.clients.forEach((client) => {
+//             if (client !== ws && client.readyState === WebSocket.OPEN) {
+//                 client.send(data, {
+//                     binary: isBinary
+//                 });
+//             }
+//         });
+//     });
+// });
 
 
 wss.on('connection', (ws) => {
@@ -57,3 +55,5 @@ wss.on('connection', (ws) => {
         }
     });
 });
+
+server.listen(8081);
