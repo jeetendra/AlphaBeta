e934caeb1066f1c6d58b103541f35ca09eaafd82
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-01-21 18:11:14 +0530

    modify peer server to store client id and broadcast it to clients

--- experiments/peer-peer-server/server.mjs
+++ experiments/peer-peer-server/server.mjs
@@ -23,36 +23,76 @@ const wss = new WebSocketServer({ server });
 //     });
 // });
 
+const clients = {};
+
 
 wss.on('connection', (ws) => {
     const id = uuidv4(); 
+    clients[id] = ws;
     console.log("Connected");
+    ws.send(JSON.stringify({ type: 'id', id: id }));
+
     ws.on('message', (message) => {
         const data = JSON.parse(message);
         console.log(data);
         switch (data.type) {
             case 'offer':
-                wss.clients.forEach((client) => {
-                    if (client !== ws && client.readyState === WebSocket.OPEN) {
-                        client.send(JSON.stringify({ type: 'offer', offer: data.offer }));
+                {
+                    const toId = data.to;
+                    const offer = data.offer;
+
+                    if (clients[toId]) {
+                        clients[toId].send(JSON.stringify({ type: 'offer', from: id, offer: offer }));
+                    } else {
+                        console.log('User not found.');
                     }
-                });
+                }
+                // wss.clients.forEach((client) => {
+                //     if (client !== ws && client.readyState === WebSocket.OPEN) {
+                //         client.send(JSON.stringify({ type: 'offer', offer: data.offer }));
+                //     }
+                // });
                 break;
             case 'answer':
-                wss.clients.forEach((client) => {
-                    if (client !== ws && client.readyState === WebSocket.OPEN) {
-                        client.send(JSON.stringify({ type: 'answer', answer: data.answer }));
+                {
+                    const toId = data.to;
+                    const answer = data.answer;
+
+                    if (clients[toId]) {
+                        clients[toId].send(JSON.stringify({ type: 'answer', from: id, answer: answer }));
+                    } else {
+                        console.log('User not found.');
                     }
-                });
+                }
+                // wss.clients.forEach((client) => {
+                //     if (client !== ws && client.readyState === WebSocket.OPEN) {
+                //         client.send(JSON.stringify({ type: 'answer', answer: data.answer }));
+                //     }
+                // });
                 break;
             case 'candidate':
-                wss.clients.forEach((client) => {
-                    if (client !== ws && client.readyState === WebSocket.OPEN) {
-                        client.send(JSON.stringify({ type: 'candidate', candidate: data.candidate }));
+                {
+                    const toId = data.to;
+                    const candidate = data.candidate;
+
+                    if (clients[toId]) {
+                        clients[toId].send(JSON.stringify({ type: 'candidate', from: id, candidate: candidate }));
+                    } else {
+                        console.log('User not found.');
                     }
-                });
+                }
+                // wss.clients.forEach((client) => {
+                //     if (client !== ws && client.readyState === WebSocket.OPEN) {
+                //         client.send(JSON.stringify({ type: 'candidate', candidate: data.candidate }));
+                //     }
+                // });
                 break;
         }
+    });
+
+    ws.on('close', () => {
+        delete clients[id];
+        console.log('Client disconnected with ID:', id);
     });
 });
 
