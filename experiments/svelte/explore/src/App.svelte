1adcd19613eed9d2f9929c71193976bb810493d2
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-01-21 23:27:01 +0530

    added typewrite component

--- experiments/svelte/explore/src/App.svelte
+++ experiments/svelte/explore/src/App.svelte
@@ -1,13 +1,16 @@
 <script>
   import Counter from './lib/Counter.svelte'
+  import TypeWriter from './lib/TypeWriter.svelte'
 </script>
 
 <main>
   <div class="card">
     <Counter />
   </div>
+  <div class="card">
+    <TypeWriter />
+  </div>
 </main>
 
 <style>
-  
 </style>
---
+++ experiments/svelte/explore/src/lib/TypeWriter.svelte
@@ -0,0 +1,11 @@
+<script>
+import {typewriter} from './actions.svelte'
+</script>
+
+<div use:typewriter= {{
+    strings: ['Hello', 'World']
+}}></div>
+
+<div use:typewriter= {{
+    strings: ['Hello', 'Another world']
+}}></div>
