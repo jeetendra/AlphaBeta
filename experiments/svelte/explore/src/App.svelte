6a1bd67f6b87fbd428513eff37b9d1e7c02a0997
Jeetendra Singh <chauhanjeetendra@gmail.com>
2025-01-22 22:33:51 +0530

    svelte: added todo component and render a list

--- experiments/svelte/explore/src/App.svelte
+++ experiments/svelte/explore/src/App.svelte
@@ -2,11 +2,15 @@
   import Counter from './lib/Counter.svelte'
   import TypeWriter from './lib/TypeWriter.svelte'
   import Swiper from './lib/Swiper.svelte';
+  import Todos from './lib/Todos.svelte';
 </script>
 
 <main>
   <div class="card">
     <Counter />
+  </div>
+  <div class="card">
+    <Todos />
   </div>
   <div class="card">
     <TypeWriter />
---
+++ experiments/svelte/explore/src/lib/Todos.svelte
@@ -0,0 +1,43 @@
+
+<script>
+ let todos = $state([
+    {text: "Hello", completed: false},
+    {text: "World", completed: true},
+ ])
+</script>
+
+<div class="todos">
+  {#each todos as todo}
+    <div class="todo">
+        <input type="text" value="{todo.text}" />
+        <input type="checkbox" checked="{todo.completed}" />
+    </div>
+  {/each}
+</div>
+
+<style>
+    .todos {
+        display: grid;
+        gap: 1rem;
+        margin-block-start: 1rem;
+        width: 400px;
+    }
+
+    .todo {
+        position: relative;
+        
+    }
+
+    input[type="text"] {
+        width: 100%;
+        padding: 1rem;
+    }
+
+
+    input[type="checkbox"] {
+        position: absolute;
+        right: -0.5rem;
+        top: 50%;
+        translate: 0% -50%;
+    }
+</style>
